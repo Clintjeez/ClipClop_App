183abfd98568c868107cfe9ce3e1068b8c60a06e
Clinton James <clintjeez@gmail.com>
2022-11-01 18:52:44 +0100

    next backend data request setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,32 @@
-import Head from 'next/head'
+import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "../types";
 
-
-export default function Home() {
-  return (
-   <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-  )
+interface IProps {
+  videos: Video[];
 }
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No Videos"} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+
+export default Home;
