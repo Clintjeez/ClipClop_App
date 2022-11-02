e4a54b6c3e5065c2867216c430e83c150e6318cc
Clinton James <clintjeez@gmail.com>
2022-11-02 23:39:16 +0100

    videocart component bug fix

---
+++ components/NoResults.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface IProps {
+  text: string;
+}
+
+const NoResults = ({ text }: IProps) => {
+  return <div>NoResult</div>;
+};
+
+export default NoResults;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import axios from "axios";
 import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
 
 interface IProps {
   videos: Video[];
